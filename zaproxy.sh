be1f428b3d166d6d65d6fa5c0d7bb304a4885f9e
kocercan <65725644+kocercan@users.noreply.github.com>
2020-06-10 13:08:37 +0300

    Update zaproxy.sh

--- zaproxy.sh
+++ zaproxy.sh
@@ -2,5 +2,5 @@
 docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py \
     -t $1  -g gen.conf -r testreport.html
 
-echo "ZAP Scaning Report Ektedir" | mail -s "ZAP Scanning Report $1" toaddr@yourdomain.com -r fromaddr@yourdomain.com -A testreport.html 
+echo "ZAP Scaning Report Ektedir" | mail -s "ZAP Scanning Report $1" toaddr@yourdomain.com -a "From: ZAProxy <zaproxy@yourdomain.com>" -A testreport.html 
 rm testreport.html

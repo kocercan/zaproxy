c12c92ee25c7c8b0ac2cec83c015460919d7d406
kocercan <65725644+kocercan@users.noreply.github.com>
2020-06-16 14:10:37 +0300

    Update zaproxy.sh

--- zaproxy.sh
+++ zaproxy.sh
@@ -4,9 +4,11 @@ input="/home/user/URL_List.txt"
 while IFS= read -r line
 do
 
-docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py \
+docker run --rm -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-baseline.py \
     -t $line  -g gen.conf -r testreport.html
 
 echo "ZAP Scaning Report Ektedir" | mail -s "ZAP Scanning Report $line" toaddr@yourdomain.com -a "From: ZAProxy <zaproxy@yourdomain.com>" -A testreport.html 
 rm testreport.html
+echo "going to sleep for 2m"
+sleep 2m
 done < "$input"
